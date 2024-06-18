db4829a11229d7701ae1657bf0ee89ee733df7d9
rayxiong <rayxiong@didiglobal.com>
2024-06-18 22:01:16 +0800

    Gson解析相关测试

--- app/src/main/java/xyz/rh/enjoyfragment/temp/TempTest3.kt
+++ app/src/main/java/xyz/rh/enjoyfragment/temp/TempTest3.kt
@@ -1,5 +1,6 @@
 package xyz.rh.enjoyfragment.temp
 
+import androidx.viewpager.widget.ViewPager
 import xyz.rh.common.log
 import xyz.rh.enjoyfragment.javacallkt.MyJavaClass
 
@@ -67,5 +68,51 @@ fun main() {
     println("map1.size = ${map1.size}")
 
 
+    val vp: ViewPager? = null
+    vp?.addOnPageChangeListener {
+//        onPageScrolled {
+//        }
+        println(1)
+        onPageSelected {
+        }
+    }
 
+}
+
+
+inline fun ViewPager.addOnPageChangeListener(func: _OnPageChangeListener.()-> Unit) {
+    val listener = _OnPageChangeListener()
+    listener.func()
+    addOnPageChangeListener(listener)
+}
+
+
+
+class _OnPageChangeListener : ViewPager.OnPageChangeListener {
+
+    private var _onPageScrollStateChanged: ((state: Int) -> Unit)? = null
+    private var _onPageScrolled: ((position: Int, positionOffset: Float, positionOffsetPixels: Int) -> Unit)? = null
+    private var _onPageSelected: ((position: Int) -> Unit)? = null
+
+
+    override fun onPageScrollStateChanged(state: Int) {
+        _onPageScrollStateChanged?.invoke(state)
+    }
+    fun onPageScrollStateChanged(func: (state: Int) -> Unit) {
+        _onPageScrollStateChanged = func
+    }
+
+    override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+        _onPageScrolled?.invoke(position, positionOffset, positionOffsetPixels)
+    }
+    fun onPageScrolled(func: (position: Int, positionOffset: Float, positionOffsetPixels: Int) -> Unit) {
+        _onPageScrolled = func
+    }
+
+    override fun onPageSelected(position: Int) {
+        _onPageSelected?.invoke(position)
+    }
+    fun onPageSelected(func: (position: Int) -> Unit) {
+        _onPageSelected = func
+    }
 }
--- app/src/main/java/xyz/rh/enjoyfragment/temp/TestCollections.kt
+++ app/src/main/java/xyz/rh/enjoyfragment/temp/TestCollections.kt
@@ -11,13 +11,35 @@ fun main() {
 
     println(123)
 
-    val intArray = mutableListOf<Int>()
-    println("size = ${intArray.size}")
-    intArray.add( 100)
-    println("get 0 = ${intArray[0]}") // get(0)返回的是size
-    println("get 1 = ${intArray[1]}")
-    AccessibilityNodeInfo.ACTION_CLICK
+    val intArray = mutableListOf<Int?>()
 
+    val intArray2 = ArrayList<Int?>()
+
+//    println("size = ${intArray.size}")
+//    intArray.add( 100)
+//    println("get 0 = ${intArray[0]}") // get(0)返回的是size
+//    println("get 1 = ${intArray[1]}")
+//    AccessibilityNodeInfo.ACTION_CLICK
+
+//    intArray.add(null)
+//    intArray.add(null)
+//    intArray.add(null)
+//    intArray.add(1)
+
+//    intArray2.add(null)
+//    intArray2.add(null)
+//    intArray2.add(null)
+
+//    println("size = ${intArray.size}")
+
+
+    val map1 = mutableMapOf<String?, Int?>()
+
+    map1.put(null, null)
+    map1.put(null, 1)
+    map1.put(null, 2)
+
+    println("size = ${map1.size}")
 
 
 }
