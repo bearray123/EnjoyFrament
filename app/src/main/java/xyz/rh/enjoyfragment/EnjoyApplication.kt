5f657b85572c43e56382301084b3cbb2fd702fff
rayxiong <rayxiong@didiglobal.com>
2024-10-14 16:37:25 +0800

    添加activity监听的生命周期

--- app/src/main/java/xyz/rh/enjoyfragment/EnjoyApplication.kt
+++ app/src/main/java/xyz/rh/enjoyfragment/EnjoyApplication.kt
@@ -1,7 +1,9 @@
 package xyz.rh.enjoyfragment
 
+import android.app.Activity
 import android.app.Application
 import android.content.Context
+import android.os.Bundle
 import android.os.Looper
 import android.util.Log
 import xyz.rh.common.log
@@ -15,6 +17,37 @@ class EnjoyApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         log("EnjoyApplication:: onCreate()")
+
+        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+                log("ActivityTrack:onActivityCreated activity:$activity")
+            }
+
+            override fun onActivityStarted(activity: Activity) {
+                log("ActivityTrack:onActivityStarted activity:$activity")
+            }
+
+            override fun onActivityResumed(activity: Activity) {
+                log("ActivityTrack:onActivityResumed activity:$activity")
+            }
+
+            override fun onActivityPaused(activity: Activity) {
+                log("ActivityTrack:onActivityPaused activity:$activity")
+
+            }
+
+            override fun onActivityStopped(activity: Activity) {
+                log("ActivityTrack:onActivityStopped activity:$activity")
+            }
+
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+            }
+
+            override fun onActivityDestroyed(activity: Activity) {
+                log("ActivityTrack:onActivityDestroyed activity:$activity")
+            }
+
+        })
 
 
     }
